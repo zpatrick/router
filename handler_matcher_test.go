53e53b843abb652b828b2313eb47c4f5f83b37ee
Zack Patrick <zpatrick319@Hotmail.com>
2018-10-01 12:18:52 -0700

    add unit tests for regex, string, and variable matchers

--- handler_matcher_test.go
+++ handler_matcher_test.go
@@ -61,7 +61,7 @@ func TestGlobHandlerMatcher(t *testing.T) {
 		},
 		"Star-mid Mismatch (spelling)": {
 			Matcher:  NewGlobHandlerMatcher("GET", "/products/*/count", nil),
-			Request:  NewRequest("GET", "/products/p123/counts"),
+			Request:  NewRequest("GET", "/products/p123/price"),
 			Expected: false,
 		},
 		"Static Match": {
@@ -86,13 +86,157 @@ func TestGlobHandlerMatcher(t *testing.T) {
 }
 
 func TestRegexHandlerMatcher(t *testing.T) {
+	cases := map[string]struct {
+		Matcher  HandlerMatcher
+		Request  *http.Request
+		Expected bool
+	}{
+		"DotStar-only Match": {
+			Matcher:  NewRegexHandlerMatcher("GET", ".*", nil),
+			Request:  NewRequest("GET", "/"),
+			Expected: true,
+		},
+		"DotStar-only Mismatch (method)": {
+			Matcher:  NewRegexHandlerMatcher("GET", ".*", nil),
+			Request:  NewRequest("PUT", "/"),
+			Expected: false,
+		},
+		"DotStar-start Match": {
+			Matcher:  NewRegexHandlerMatcher("GET", ".*/products", nil),
+			Request:  NewRequest("GET", "/api/v1/products"),
+			Expected: true,
+		},
+		"DotStar-start Mismatch (spelling)": {
+			Matcher:  NewRegexHandlerMatcher("GET", ".*/products", nil),
+			Request:  NewRequest("GET", "/api/v1/product"),
+			Expected: false,
+		},
+		"DotStar-end Match": {
+			Matcher:  NewRegexHandlerMatcher("GET", "/products/.*", nil),
+			Request:  NewRequest("GET", "/products/p123"),
+			Expected: true,
+		},
+		"DotStar-end Mismatch (too short)": {
+			Matcher:  NewRegexHandlerMatcher("GET", "/products/.*", nil),
+			Request:  NewRequest("GET", "/products"),
+			Expected: false,
+		},
+		"DotStar-mid Match": {
+			Matcher:  NewRegexHandlerMatcher("GET", "/products/.*/count", nil),
+			Request:  NewRequest("GET", "/products/p123/count"),
+			Expected: true,
+		},
+		"DotStar-mid Mismatch (spelling)": {
+			Matcher:  NewRegexHandlerMatcher("GET", "/products/.*/count", nil),
+			Request:  NewRequest("GET", "/products/p123/price"),
+			Expected: false,
+		},
+		"Static Match": {
+			Matcher:  NewRegexHandlerMatcher("GET", "/products", nil),
+			Request:  NewRequest("GET", "/products"),
+			Expected: true,
+		},
+		"Static Mismatch (spelling)": {
+			Matcher:  NewRegexHandlerMatcher("GET", "/products", nil),
+			Request:  NewRequest("GET", "/product"),
+			Expected: false,
+		},
+	}
 
+	for name, c := range cases {
+		t.Run(name, func(t *testing.T) {
+			if _, result := c.Matcher(c.Request); result != c.Expected {
+				t.Errorf("Result was %v, expected %v", result, c.Expected)
+			}
+		})
+	}
 }
 
 func TestStringHandlerMatcher(t *testing.T) {
+	cases := map[string]struct {
+		Matcher  HandlerMatcher
+		Request  *http.Request
+		Expected bool
+	}{
+		"Static Match": {
+			Matcher:  NewStringHandlerMatcher("GET", "/products", nil),
+			Request:  NewRequest("GET", "/products"),
+			Expected: true,
+		},
+		"Static Mismatch (method)": {
+			Matcher:  NewStringHandlerMatcher("GET", "/products", nil),
+			Request:  NewRequest("PUT", "/product"),
+			Expected: false,
+		},
+		"Static Mismatch (spelling)": {
+			Matcher:  NewStringHandlerMatcher("GET", "/products", nil),
+			Request:  NewRequest("GET", "/product"),
+			Expected: false,
+		},
+	}
 
+	for name, c := range cases {
+		t.Run(name, func(t *testing.T) {
+			if _, result := c.Matcher(c.Request); result != c.Expected {
+				t.Errorf("Result was %v, expected %v", result, c.Expected)
+			}
+		})
+	}
 }
 
 func TestVariableHandlerMatcher(t *testing.T) {
+	cases := map[string]struct {
+		Matcher  HandlerMatcher
+		Request  *http.Request
+		Expected bool
+	}{
+		"Variable-only": {
+			Matcher:  NewVariableHandlerMatcher("GET", "/:var", nil),
+			Request:  NewRequest("GET", "/products"),
+			Expected: true,
+		},
+		"Variable-only Mismatch (method)": {
+			Matcher:  NewVariableHandlerMatcher("GET", "/:var", nil),
+			Request:  NewRequest("PUT", "/products"),
+			Expected: false,
+		},
+		"Variable": {
+			Matcher:  NewVariableHandlerMatcher("GET", "/products/:productID", nil),
+			Request:  NewRequest("GET", "/products/p123"),
+			Expected: true,
+		},
+		"Variable mismatch (spelling)": {
+			Matcher:  NewVariableHandlerMatcher("GET", "/products/:productID", nil),
+			Request:  NewRequest("GET", "/product/p123"),
+			Expected: false,
+		},
+		"Variable mismatch (too short)": {
+			Matcher:  NewVariableHandlerMatcher("GET", "/products/:productID", nil),
+			Request:  NewRequest("GET", "/products"),
+			Expected: false,
+		},
+		"Variable mismatch (too long)": {
+			Matcher:  NewVariableHandlerMatcher("GET", "/products/:productID", nil),
+			Request:  NewRequest("GET", "/product/p123/price"),
+			Expected: false,
+		},
+		"Static Match": {
+			Matcher:  NewVariableHandlerMatcher("GET", "/products", nil),
+			Request:  NewRequest("GET", "/products"),
+			Expected: true,
+		},
+		"Static Mismatch (spelling)": {
+			Matcher:  NewVariableHandlerMatcher("GET", "/products", nil),
+			Request:  NewRequest("GET", "/product"),
+			Expected: false,
+		},
+	}
 
+	for name, c := range cases {
+		t.Run(name, func(t *testing.T) {
+			if _, result := c.Matcher(c.Request); result != c.Expected {
+				t.Errorf("Result was %v, expected %v", result, c.Expected)
+			}
+		})
+	}
 }
